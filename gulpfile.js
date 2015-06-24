4645e409799798704945669b30ba99203a336797
AlphaTech <alphatech@btri.x10.bz>
2015-06-24 17:55:34 -0400

    Test Files

---
+++ gulpfile.js
@@ -0,0 +1,71 @@
+#! /usr/bin/env node
+
+(function () {
+    'use strict';
+
+    var define = require('amdefine')(module);
+
+    var deps = [
+        'gulp',
+        'gulp-jsdoc',
+        'gulp-jshint',
+        'gulp-mocha',
+        'gulp-istanbul',
+        'gulp-foreach',
+    ];
+
+    define(deps, function(config, gulp, jsdoc, jshint, mocha, istanbul, foreach) {
+        var src = [
+            'lib/*.js'
+        ];
+
+        gulp.task('lint', [], function () {
+            return gulp.src(src)
+                .pipe(jshint())
+                .pipe(jshint.reporter('default'));
+        });
+
+        gulp.task('jsdoc', [], function () {
+            return gulp.src(src)
+                .pipe(jsdoc.parser())
+                .pipe(jsdoc.generator('data/doc/'));
+        });
+
+        gulp.task('mocha', [], function (cb) {
+            var ciMode = (process.env.CI === 'true');
+            process.env.BUILD_ENVIRONMENT = 'test';
+            gulp.src(['./*.js', './lib/**/*.js', './test/mocks/**/*.js'])
+                .pipe(istanbul({
+                  includeUntested: true,
+                  reporters: [ 'lcov' ],
+                }))
+                .pipe(istanbul.hookRequire())
+                .on('finish', function () {
+                return gulp.src('tests/unit/**/*.js', { read: false })
+                    .pipe(mocha({
+                      reporter: ((!ciMode && config.test_reporter) || 'spec'),
+                    }))
+                    .pipe(istanbul.writeReports())
+                    .on('end', cb);
+                });
+        });
+
+        // Build Task
+        gulp.task('build', [
+            'lint',
+            'jsdoc',
+            'test'
+        ]);
+
+        // Build Task
+        gulp.task('test', [
+            'mocha'
+        ]);
+
+        // Default Task
+        gulp.task('default', [
+            'build'
+        ]);
+
+    });
+}());
