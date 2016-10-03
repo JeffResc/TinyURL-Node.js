ee8dc2495789c768efbe70ebe3dc8f45cada3f12
Yoni Stap <yoni.stap@gmail.com>
2016-10-04 00:35:03 +0200

    refactored code to use new package

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var needle = require('needle')
 
 module.exports = {
     shorten: function(url, cb) {
-        request('http://tinyurl.com/api-create.php?url=' + encodeURIComponent(url), function (error, response, body) {
-			cb(body.split("\n")[0]);
+        needle.get('http://tinyurl.com/api-create.php?url=' + encodeURIComponent(url), function (error, response) {
+			cb(response.body.split("\n")[0]);
         });
     }
 };
