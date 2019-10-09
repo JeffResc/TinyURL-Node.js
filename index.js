3cbcca6781f3b6f0534a95ff5c24979a8f5daf77
Ahmed Karaman <20889958+ahmedkrmn@users.noreply.github.com>
2019-10-09 03:49:59 +0200

    Implement Tiny-URL link resolver

--- index.js
+++ index.js
@@ -21,5 +21,27 @@ module.exports = {
 				})
 			})
 		}
-	}
+	},
+	resolve: function(url, cb) {
+		if (typeof cb === "function")
+		  http
+			.get(url, res => {
+			  if (res.headers.location) cb(res.headers.location);
+			  else cb(null, new Error("Tiny URL not found!"));
+			})
+			.on("error", err => {
+			  cb(null, err);
+			});
+		else
+		  return new Promise((resolve, reject) => {
+			http
+			  .get(url, res => {
+				if (res.headers.location) resolve(res.headers.location);
+				else reject(new Error("Tiny URL not found!"));
+			  })
+			  .on("error", err => {
+				reject(err);
+			  });
+		  });
+	  }
 };
